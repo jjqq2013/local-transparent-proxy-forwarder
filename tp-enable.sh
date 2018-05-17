87999fb41614f02366bb6c8d419ee65df4b311af
q <osexp2000@gmail.com>
2018-05-17 16:03:29 +0900

    args

--- tp-enable.sh
+++ tp-enable.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+PROXY_HOST=${1?"require an argument as HTTP proxy host"}; shift
+BYPASS_IP_SEGMENTS=${1?"require an argument as bypass IP segments, example: 100.99.88.0/24"}; shift
+OUTPUT_INTERFACE=${1?"require an argument as output interface, example: eth0"}; shift
+
 if ! sudo iptables -t nat -n --list tp-OUTPUT >/dev/null 2>&1; then
   sudo iptables -t nat --new-chain tp-OUTPUT
 fi
@@ -12,12 +16,12 @@ sudo iptables -t nat --append OUTPUT -j tp-OUTPUT
 sudo iptables -t nat --append PREROUTING -j tp-PREROUTING
 
 #add some bypass ip
-sudo iptables -t nat --append tp-OUTPUT -d 100.88.77.66 -j RETURN
-sudo iptables -t nat --append tp-OUTPUT -d 100.99.88.0/24 -j RETURN
+sudo iptables -t nat --append tp-OUTPUT -d "$PROXY_HOST" -j RETURN
+sudo iptables -t nat --append tp-OUTPUT -d "$BYPASS_IP_SEGMENTS" -j RETURN
 
-sudo iptables -t nat --append tp-OUTPUT -o eth0 -p tcp --dport 443 -m tcp -j REDIRECT --to-ports 10080
-sudo iptables -t nat --append tp-OUTPUT -o eth0 -p tcp --dport 80 -m tcp -j REDIRECT --to-ports 10081
+sudo iptables -t nat --append tp-OUTPUT -o "$OUTPUT_INTERFACE" -p tcp --dport 443 -m tcp -j REDIRECT --to-ports 10080
+sudo iptables -t nat --append tp-OUTPUT -o "$OUTPUT_INTERFACE" -p tcp --dport 80 -m tcp -j REDIRECT --to-ports 10081
 
 #this is for process inside docker container
-sudo iptables -t nat --append tp-PREROUTING ! -i eth0 -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 10080
-sudo iptables -t nat --append tp-PREROUTING ! -i eth0 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 10081
+sudo iptables -t nat --append tp-PREROUTING ! -i "$OUTPUT_INTERFACE" -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 10080
+sudo iptables -t nat --append tp-PREROUTING ! -i "$OUTPUT_INTERFACE" -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 10081
--- tp-install.sh
+++ tp-install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 git clone https://github.com/jjqq2013/pr0cks pr0cks
 curl -o node-v9.3.0-linux-x64.tar.xz https://nodejs.org/dist/v9.3.0/node-v9.3.0-linux-x64.tar.xz && tar xJf node-v9.3.0-linux-x64.tar.xz
 git clone https://github.com/jjqq2013/ForwardHttpToProxy
